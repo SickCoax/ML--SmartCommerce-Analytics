8df7706f482062fb47f2d1f8f8acdee3b80421d7
Sahil Joshi <sail.joshi0604@gmail.com>
2026-04-15 23:26:46 +0530

    aggregation

--- src/data_load.py
+++ src/data_load.py
@@ -8,4 +8,24 @@ sessions = pd.read_csv(r"C:\Users\sailj\OneDrive\文档\GitHub\SmartCommerce Ana
 df = pd.merge(transactions , customers , on="customer_id")
 df = pd.merge(df , products , on="product_id")
 
-df = df.drop(["transaction_id" , "customer_id" , "product_id" , "transaction_date" , "signup_date" , "product_name" , "stock_quantity" , "weight_kg"] , axis=1)
+df = df.drop(["transaction_id"  , "product_id" , "transaction_date" , "signup_date" , "product_name" , "stock_quantity" , "weight_kg"] , axis=1)
+print(df)
+
+df = df.groupby("customer_id").agg({
+    "quantity" : "sum" ,
+    "unit_price" : "mean" ,
+    "total_amount" : "sum" ,
+    "discount_applied" : ["sum" , "mean"] ,
+    "shipping_cost" : ["sum" , "mean"] ,
+    "age" : "max" ,
+    
+})
+
+
+
+# status
+# payment method
+# gender
+# country
+# segment
+# 
