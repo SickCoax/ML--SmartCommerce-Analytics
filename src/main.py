570f99419f0e0bc4c0c134777fa3060f7952e029
Sahil Joshi <sail.joshi0604@gmail.com>
2026-06-05 04:50:11 +0530

    improved file locations and used optimal file location

--- src/main.py
+++ src/main.py
@@ -4,21 +4,11 @@ from train_regression import train_lifetime_value
 from train_classification import train_is_churned
 from evaluation import evaluate_reg , evaluate_cls
 
-customers = pd.read_csv(r"C:\Users\sailj\OneDrive\文档\GitHub\SmartCommerce Analytics\dataset\customers.csv")
-transactions = pd.read_csv(r"C:\Users\sailj\OneDrive\文档\GitHub\SmartCommerce Analytics\dataset\transactions.csv")
-products = pd.read_csv(r"C:\Users\sailj\OneDrive\文档\GitHub\SmartCommerce Analytics\dataset\products.csv")
+customers = pd.read_csv(r"dataset\customers.csv")
+transactions = pd.read_csv(r"dataset\transactions.csv")
+products = pd.read_csv(r"dataset\products.csv")
 
 df = processed_dataset(customers , transactions , products)
-
-
-
-# model , X_test , y_test = train_is_churned(df)
-
-# ypred , f1 = evaluate_cls(model , X_test , y_test)
-
-# print(ypred)
-# print()
-# print(f1)
 
 while True :
 
